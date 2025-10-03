d93f65d234c59f3c91baef36a5d7960a88c8c518
rezaginting2312 <rezabrema557@gmail.com>
2025-10-03 10:44:58 +0700

    upload project laravel

---
+++ www/LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak - LaraPress</title>
+</head>
+<body>
+    <h1>Hubungi Kami</h1>
+    <p>Jika ada pertanyaan, silakan hubungi kami melalui kontak berikut:</p>
+
+    <ul>
+        <li>Email: kontak@larapress.test</li>
+        <li>Telepon: +62 812 3422 5678</li>
+    </ul>
+</body>
+</html>
+    <a href="/">Kembali ke Halaman Utama</a>
+    <a href="/tentang-kami">Tentang Kami</a>
